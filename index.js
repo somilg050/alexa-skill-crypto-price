3d8385cc51f7172bd175ae0431f3e1af19f1a286
Somil Gupta <f20170142@goa.bits-pilani.ac.in>
2019-08-30 14:09:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 const Alexa = require('ask-sdk-core');
 
+var repeatStatement;
 const GetRemoteDataHandler = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === 'LaunchRequest'
@@ -10,15 +11,36 @@ const GetRemoteDataHandler = {
       && handlerInput.requestEnvelope.request.intent.name === 'GetRemoteDataIntent');
   },
   async handle(handlerInput) {
-    let outputSpeech = 'This is the default message.';
+    let outputSpeech = 'Welcome to Crypto Price! I can tell you the current price of Bitcoin and Ethereum. Which one would you like to know?';
+    repeatStatement = outputSpeech;
     const request = handlerInput.requestEnvelope.request;
-    var currency=0;
-    if(request.intent.slots.currency.hasOwnProperty("resolutions")){
-      currency = request.intent.slots.currency.resolutions.resolutionsPerAuthority[0].values[0].value.name;
+    var currency=0,crypto=0;
+    if(handlerInput.requestEnvelope
+      && handlerInput.requestEnvelope.request
+      && handlerInput.requestEnvelope.request.intent
+      && handlerInput.requestEnvelope.request.intent.slots
+      && handlerInput.requestEnvelope.request.intent.slots.currency
+      && handlerInput.requestEnvelope.request.intent.slots.currency.resolutions
+      && handlerInput.requestEnvelope.request.intent.slots.currency.resolutions.resolutionsPerAuthority[0]
+      && (handlerInput.requestEnvelope.request.intent.slots.currency.resolutions.resolutionsPerAuthority[0].status.code === "ER_SUCCESS_MATCH")){
+        currency = request.intent.slots.currency.resolutions.resolutionsPerAuthority[0].values[0].value.name;
     }
-    console.log(currency);
-    var crypto = request.intent.slots.cryptoCurrency.resolutions.resolutionsPerAuthority[0].values[0].value.name;
-    console.log(crypto);
+    if(handlerInput.requestEnvelope
+      && handlerInput.requestEnvelope.request
+      && handlerInput.requestEnvelope.request.intent
+      && handlerInput.requestEnvelope.request.intent.slots
+      && handlerInput.requestEnvelope.request.intent.slots.cryptoCurrency
+      && handlerInput.requestEnvelope.request.intent.slots.cryptoCurrency.resolutions
+      && handlerInput.requestEnvelope.request.intent.slots.cryptoCurrency.resolutions.resolutionsPerAuthority[0]
+      && (handlerInput.requestEnvelope.request.intent.slots.cryptoCurrency.resolutions.resolutionsPerAuthority[0].status.code === "ER_SUCCESS_MATCH")){
+        crypto = request.intent.slots.cryptoCurrency.resolutions.resolutionsPerAuthority[0].values[0].value.name;
+    }
+    // if(request.intent.slots.currency.hasOwnProperty("resolutions")){
+    //   currency = request.intent.slots.currency.resolutions.resolutionsPerAuthority[0].values[0].value.name;
+    // }
+    // if(request.intent.slots.cryptoCurrency.hasOwnProperty("resolutions")){
+    //   crypto = request.intent.slots.cryptoCurrency.resolutions.resolutionsPerAuthority[0].values[0].value.name;
+    // }
     await getRemoteData('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsyms=USD,EUR&api_key=9d6d0ad237ed6e36e7b9f60ea1590244f25f201445320f188bdda2f8dd237121')
       .then((response) => {
         const data = JSON.parse(response);
@@ -32,8 +54,10 @@ const GetRemoteDataHandler = {
           else{
             outputSpeech = `Current Bitcoin price is ${data.BTC.USD} dollar.`;
           }
+          outputSpeech+= ` Do you need the price of another crypto? `;
+          repeatStatement = outputSpeech;
         }
-        else{
+        if(crypto==='ETH'){
           if(currency==="USD"){
             outputSpeech = `Current Ethereum price is ${data.ETH.USD} dollar.`;
           }
@@ -43,6 +67,8 @@ const GetRemoteDataHandler = {
           else{
             outputSpeech = `Current Ethereum price is ${data.ETH.USD} dollar.`;
           }
+          outputSpeech+= ` Do you need the price of another crypto? `;
+          repeatStatement = outputSpeech;
         }
       })
       .catch((err) => {
@@ -52,6 +78,7 @@ const GetRemoteDataHandler = {
 
     return handlerInput.responseBuilder
       .speak(outputSpeech)
+      .reprompt(`I can tell you the current price of Bitcoin and Ethereum. What can I help you with?`)
       .withShouldEndSession(false)
       .getResponse();
 
@@ -64,8 +91,8 @@ const HelpIntentHandler = {
       && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
   },
   handle(handlerInput) {
-    const speechText = 'You can introduce yourself by telling me your name';
-
+    const speechText = `I can tell you the current price of Bitcoin and Ethereum. What can I help you with?`;
+    repeatStatement = speechText;
     return handlerInput.responseBuilder
       .speak(speechText)
       .reprompt(speechText)
@@ -84,6 +111,7 @@ const CancelAndStopIntentHandler = {
 
     return handlerInput.responseBuilder
       .speak(speechText)
+      .withShouldEndSession(true)
       .getResponse();
   },
 };
@@ -99,6 +127,38 @@ const SessionEndedRequestHandler = {
   },
 };
 
+const YesHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && request.intent.name === 'AMAZON.YesIntent';
+  },
+  handle(handlerInput) {
+    var speechOutput = 'Great! Which crypto do you need the price of? Bitcoin or Ether?';
+    repeatStatement = speechOutput;
+    return handlerInput.responseBuilder
+      .speak(speechOutput)
+      .reprompt("Which crypto do you need the price of? Bitcoin or Ether?")
+      .withShouldEndSession(false)
+      .getResponse();
+  },
+};
+
+const NoHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && request.intent.name === 'AMAZON.NoIntent';
+  },
+  handle(handlerInput) {
+    var speechOutput = 'Goodbye and have a nice day';
+    return handlerInput.responseBuilder
+      .speak(speechOutput)
+      .withShouldEndSession(true)
+      .getResponse();
+  },
+};
+
 const ErrorHandler = {
   canHandle() {
     return true;
@@ -107,8 +167,21 @@ const ErrorHandler = {
     console.log(`Error handled: ${error.message}`);
 
     return handlerInput.responseBuilder
-      .speak('Sorry, I can\'t understand the command. Please say again.')
-      .reprompt('Sorry, I can\'t understand the command. Please say again.')
+      .speak(`Sorry, I didn't get that. Try asking about Bitcoin or Ethereum.`)
+      .reprompt(`Sorry, I didn't get that. Try asking about Bitcoin or Ethereum.`)
+      .getResponse();
+  },
+};
+
+const RepeatHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest' && (request.intent.name === 'AMAZON.RepeatIntent');
+  },
+  handle(handlerInput) {
+    return handlerInput.responseBuilder
+      .speak(repeatStatement)
+      .withShouldEndSession(false)
       .getResponse();
   },
 };
@@ -135,7 +208,10 @@ exports.handler = skillBuilder
     GetRemoteDataHandler,
     HelpIntentHandler,
     CancelAndStopIntentHandler,
-    SessionEndedRequestHandler
+    SessionEndedRequestHandler,
+    RepeatHandler,
+    YesHandler,
+    NoHandler
   )
   .addErrorHandlers(ErrorHandler)
   .lambda();
